6bf3a4ad88cf54a025ac144a23b2527365e71fe8
Wardiyono <wardiyono@wardiyono-ThinkPad-Edge-E125>
2014-06-26 12:27:22 +0700

    Frontend data functions

--- frontend/sysconfig.inc.php
+++ frontend/sysconfig.inc.php
@@ -186,7 +186,7 @@ if (!defined('DB_HOST')) { define('DB_HOST', 'localhost'); }
 if (!defined('DB_PORT')) { define('DB_PORT', '3306'); }
 if (!defined('DB_NAME')) { define('DB_NAME', 'db_jkwjk3'); }
 if (!defined('DB_USERNAME')) { define('DB_USERNAME', 'root'); }
-if (!defined('DB_PASSWORD')) { define('DB_PASSWORD', ''); }
+if (!defined('DB_PASSWORD')) { define('DB_PASSWORD', 'wardi1270'); }
 
 // database connection
 // we prefer to use mysqli extensions if its available
